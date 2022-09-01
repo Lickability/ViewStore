945f9ea5be37520bfd3e23723f40eacf5dfd0af2
Twig <aharris.1990@gmail.com>
2022-09-01 11:57:36 -0400

    Adds a publisher option to the binding extension

--- Sources/ViewStore/ViewStore+BindingAdditions.swift
+++ Sources/ViewStore/ViewStore+BindingAdditions.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import CasePaths
+import Combine
 
 /// An extension on `ViewStore` that provides conveniences for creating `Binding`s.
 public extension ViewStore {
@@ -44,6 +45,30 @@ public extension ViewStore {
             self.viewState[keyPath: viewStateKeyPath] != nil
         } set: { value in
             self.send(actionCasePath.embed(()))
+        }
+    }
+    
+    /// Provides a mechanism for creating `Binding`s that send their value to a `PassthroughSubject`, leveraging `KeyPath`s to reduce duplication and errors.
+    /// - Parameters:
+    ///   - viewStateKeyPath: The `KeyPath` to the `ViewState` property that this binding wraps.
+    ///   - publisher: The publisher to send the value to.
+    func makeBinding<Value>(viewStateKeyPath: KeyPath<ViewState, Value>, publisher: PassthroughSubject<Value, Never>) -> Binding<Value> {
+        return .init {
+            self.viewState[keyPath: viewStateKeyPath]
+        } set: { value in
+            publisher.send(value)
+        }
+    }
+    
+    /// Provides a mechanism for creating `Binding`s that send their value to a `CurrentValueSubject`, leveraging `KeyPath`s to reduce duplication and errors.
+    /// - Parameters:
+    ///   - viewStateKeyPath: The `KeyPath` to the `ViewState` property that this binding wraps.
+    ///   - publisher: The publisher to send the value to.
+    func makeBinding<Value>(viewStateKeyPath: KeyPath<ViewState, Value>, publisher: CurrentValueSubject<Value, Never>) -> Binding<Value> {
+        return .init {
+            self.viewState[keyPath: viewStateKeyPath]
+        } set: { value in
+            publisher.send(value)
         }
     }
 }
