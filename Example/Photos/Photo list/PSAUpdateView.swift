244a3d7922fcd48cdb07d7d960a23f3e372786e6
Ken Ackerson <ken@lickability.net>
2023-04-03 12:12:41 -0400

    Create new view store for the update screen

--- Example/Photos/Photo list/PSAUpdateView.swift
+++ Example/Photos/Photo list/PSAUpdateView.swift
@@ -7,8 +7,45 @@
 
 import Foundation
 import SwiftUI
+import Combine
+
+final class PSAUpdateViewStore: ViewStore {
+    
+    @Published var viewState: ViewState
+    var publishedViewState: AnyPublisher<ViewState, Never> {
+        return $viewState.eraseToAnyPublisher()
+    }
+    
+    private let psaViewStore: any PSAViewStoreType
+    
+    init(psaViewStore: any PSAViewStoreType) {
+        self.psaViewStore = psaViewStore
+        
+        viewState = ViewState(psaViewState: psaViewStore.viewState)
+        psaViewStore.publishedViewState.map(ViewState.init).assign(to: &$viewState)
+    }
+    
+    struct ViewState {
+        let psaViewState: PSAViewStore.ViewState
+    }
+    
+    enum Action {
+        
+    }
+    
+    func send(_ action: Action) {
+        
+    }
+    
+    
+}
 
 struct PSAUpdateView: View {
+    
+    
+    init() {
+        
+    }
     
     var body: some View {
         EmptyView()
