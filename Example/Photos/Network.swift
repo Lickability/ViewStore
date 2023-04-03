c3227801f0604dc6813082c493de1e2bb994548a
Ken Ackerson <ken@lickability.net>
2023-04-03 14:31:32 -0400

    Move networking

---
+++ Example/Photos/Network.swift
@@ -0,0 +1,61 @@
+//
+//  Network.swift
+//  ViewStore
+//
+//  Created by Kenneth Ackerson on 4/3/23.
+//
+
+import Foundation
+import Combine
+
+final class Network {
+    
+    enum NetworkState {
+        case notStarted
+        case inProgress
+        case finished(Result<PSA, NSError>)
+        
+        var psa: PSA? {
+            switch self {
+            case .inProgress, .notStarted:
+                return nil
+            case .finished(let result):
+                return try? result.get()
+            }
+        }
+        
+        var error: Error? {
+            switch self {
+            case .notStarted, .inProgress:
+                return nil
+            case .finished(let result):
+                do {
+                    _ = try result.get()
+                    return nil
+                }
+                catch {
+                    return error
+                }
+            }
+        }
+    }
+    
+    var publisher: PassthroughSubject<NetworkState, Never> = .init()
+    
+    func request(psa: PSA) {
+        self.publisher.send(.inProgress)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            //self.publisher.send(.finished(.success(psa)))
+            
+            self.publisher.send(.finished(.failure(.init())))
+
+        }
+    
+    }
+    
+    func reset() {
+        self.publisher.send(.notStarted)
+    }
+    
+}
