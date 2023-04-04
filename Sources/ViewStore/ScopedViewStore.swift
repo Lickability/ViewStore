b6848080788da96bc9c0f080d402b0dcd3fb57cf
Ken Ackerson <ken@lickability.net>
2023-04-04 16:28:52 -0400

    making things public

--- Sources/ViewStore/ScopedViewStore.swift
+++ Sources/ViewStore/ScopedViewStore.swift
@@ -9,28 +9,28 @@ import Foundation
 import Combine
 import CasePaths
 
-final class ScopedViewStore<ViewState, Action>: ViewStore {
-    @Published var viewState: ViewState
+public final class ScopedViewStore<ViewState, Action>: ViewStore {
+    @Published public var viewState: ViewState
     
-    var publishedViewState: AnyPublisher<ViewState, Never> {
+    public var publishedViewState: AnyPublisher<ViewState, Never> {
         return $viewState.eraseToAnyPublisher()
     }
 
     private let action: (Action) -> Void
     
-    init(initial: ViewState, viewStatePub: some Publisher<ViewState, Never>, action: @escaping (Action) -> Void) {
+    public init(initial: ViewState, viewStatePub: some Publisher<ViewState, Never>, action: @escaping (Action) -> Void) {
         viewState = initial
         self.action = action
         viewStatePub.assign(to: &$viewState)
     }
 
-    func send(_ action: Action) {
+    public func send(_ action: Action) {
         self.action(action)
     }
 
 }
 
-extension ViewStore {
+public extension ViewStore {
     func scoped<Substate, Subaction>(viewStateKeyPath: KeyPath<ViewState, Substate>, actionCasePath: CasePath<Action, Subaction>) -> any ViewStore<Substate, Subaction> {
         return ScopedViewStore(initial: viewState[keyPath: viewStateKeyPath], viewStatePub: publishedViewState.map(viewStateKeyPath), action: { self.send(actionCasePath.embed($0)) })
     }
