b245171231427e93ef7559de706485c00a620107
Ken Ackerson <ken@lickability.net>
2023-05-16 10:42:44 -0400

    feedback !

--- Example/Photos/With Stores/PhotoListViewStore.swift
+++ Example/Photos/With Stores/PhotoListViewStore.swift
@@ -85,7 +85,6 @@ final class PhotoListViewStore: Store {
         }
     }
     
-    /// Action enum represents the various actions that can be performed on the State
     enum Action {
         /// Toggle the display of photo count
         case toggleShowsPhotoCount(Bool)
@@ -161,17 +160,17 @@ final class PhotoListViewStore: Store {
 
 extension PhotoListViewStoreType {
     
-    /// Computed property that provides a scoped BannerDataStoreType instance
+    /// Computed property that provides a scoped `BannerDataStoreType` instance
     var bannerDataStore: any BannerDataStoreType {
         return scoped(stateKeyPath: \.bannerState, actionCasePath: /Action.bannerAction)
     }
     
-    /// Computed property that creates a binding for the showUpdateView state
+    /// Computed property that creates a binding for the `showUpdateView` state
     var showUpdateView: Binding<Bool> {
         makeBinding(stateKeyPath: \.showUpdateView, actionCasePath: /PhotoListViewStore.Action.showUpdateView)
     }
     
-    /// Computed property that creates a binding for the showsPhotoCount state
+    /// Computed property that creates a binding for the `showsPhotoCount` state
     var showsPhotoCount: Binding<Bool> {
 //
 //        return Binding<Bool> {
@@ -184,7 +183,7 @@ extension PhotoListViewStoreType {
         makeBinding(stateKeyPath: \.showsPhotoCount, actionCasePath: /Action.toggleShowsPhotoCount)
     }
 
-    /// Computed property that creates a binding for the searchText state
+    /// Computed property that creates a binding for the `searchText` state
     var searchText: Binding<String> {
         makeBinding(stateKeyPath: \.searchText, actionCasePath: /Action.search)
     }
